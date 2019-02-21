8e50a28cc928b3060894eb3f2ada3fac6047706a
joepound <14357797+joepound@users.noreply.github.com>
2019-02-21 15:23:59 -0700

    Enabled foreign key constraints for API

--- data/knexfile.js
+++ data/knexfile.js
@@ -3,25 +3,40 @@ module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./lambda.sqlite3"
+      filename: "./recipe-book.sqlite3"
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    }
   },
 
   // For actual dev use
   devServer: {
     client: "sqlite3",
     connection: {
-      filename: "./data/lambda.sqlite3"
+      filename: "./data/recipe-book.sqlite3"
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    }
   },
 
   production: {
     client: "sqlite3",
     connection: {
-      filename: "./data/lambda.sqlite3"
+      filename: "./data/recipe-book.sqlite3"
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    }
   }
 };
